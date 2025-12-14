de1ba126e6e0efacbb0b027ae7aaaa16c9739e05
Tomsa Luca <lucasebastian777@gmail.com>
2025-12-14 23:47:22 +0200

    add csv to sqlite database conversion

--- src/main/java/com/Main.java
+++ src/main/java/com/Main.java
@@ -1,7 +1,16 @@
 package com;
 
+import com.utils.DatabaseImporter;
+
 public class Main {
     public static void main(String[] args) {
+        String csvFile = "src/main/resources/wdbc.csv";
+        String dbFile = "data/breast_cancer_data.db";
 
+        try {
+            DatabaseImporter.convertCsvToSqlite(csvFile, dbFile, "cancer_data");
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
