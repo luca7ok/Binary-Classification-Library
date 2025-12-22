086d669c2eaac8a6a517ab16302d4a2ae49af95e
Tomsa Luca <lucasebastian777@gmail.com>
2025-12-22 17:04:31 +0200

    update label encoder logic

--- src/main/java/com/utils/LabelEncoder.java
+++ src/main/java/com/utils/LabelEncoder.java
@@ -2,10 +2,7 @@ package com.utils;
 
 import com.domain.Instance;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class LabelEncoder {
     public static List<Instance<Double, Double>> encode(List<Instance<Double, String>> rawData) {
@@ -13,12 +10,17 @@ public class LabelEncoder {
         Map<String, Double> map = new HashMap<>();
         double nextCode = 0.0;
 
+        Set<String> uniqueLabels = new TreeSet<>();
         for (Instance<Double, String> instance : rawData) {
-            String label = instance.getOutput();
-            if (!map.containsKey(label)) {
-                map.put(label, nextCode++);
-            }
-            numericData.add(new Instance<>(instance.getInput(), map.get(label)));
+            uniqueLabels.add(instance.getOutput());
+        }
+
+        for (String label : uniqueLabels) {
+            map.put(label, nextCode++);
+        }
+
+        for (Instance<Double, String> instance : rawData) {
+            numericData.add(new Instance<>(instance.getInput(), map.get(instance.getOutput())));
         }
         return numericData;
     }
